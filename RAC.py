d53f37ac25b0bd2eb57380283e7234ccd327a274
rafaelt88@gmail.com <rafaelt88@gmail.com@a2e7d75b-8082-3926-6984-c9d08f189f55>
2012-05-17 09:53:54 +0000

    SEMANA 4 (ENTREGA TECNICA)

---
+++ RAC.py
@@ -0,0 +1,16 @@
+import aiml
+
+def main():
+         
+    rac = aiml.Kernel()
+    rac.learn("RAC.xml")
+    rac.respond("LOAD AIML")
+    
+    while True:
+        usr = raw_input("Usuario: ")
+        bot = rac.respond(usr)
+        print "RAC: " + bot 
+
+if __name__ == '__main__':
+    print "Bienvenido al Sistema"
+    main()
