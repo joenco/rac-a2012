97ef7f54fbd04fca0573768b7f3ce070a6f86499
rennolaj@gmail.com <rennolaj@gmail.com@a2e7d75b-8082-3926-6984-c9d08f189f55>
2012-05-31 06:20:22 +0000

    Menu con texto

--- ambiente/PruebaMenu.py
+++ ambiente/PruebaMenu.py
@@ -15,6 +15,16 @@ class javier(wx.Frame):
         menubar.Append(file,"File")
         menubar.Append(edit,"Edit")
         self.SetMenuBar(menubar)
+        #ESTA PARTE DEL CODIGO CREA UN BOX PARA ESCRIBIR PRO LO HACE EN UNA VENTANA APARTE HAY QUE VER COMO AGREGARSELA A LA VENTANA PRINCIPAL
+        box = wx.TextEntryDialog(self,"Write","write in here")
+        #wx.TextEntryDialog(panel,"Write","write in here")        
+        #if box.ShowModal() == wx.ID_OK:
+        #    answer=box.GetValue()
+        #custom = wx.StaticText(panel,-1,answer,(10,10),(300,200),wx.ALIGN_CENTER)
+        custom = wx.StaticText(panel,-1,"Buenas noches",(10,10),(300,200),wx.ALIGN_CENTER)
+        custom.SetForegroundColour('black')
+        custom.SetBackgroundColour('white')
+        
         
 if __name__=='__main__':
     app=wx.PySimpleApp()
